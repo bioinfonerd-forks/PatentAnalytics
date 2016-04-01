bfb5df792866741f9be0535ba0e817fef10ae238
ermlickw <ermlickw@vt.edu>
2016-04-01 13:59:05 -0400

    Added files via upload

--- test.py
+++ test.py
@@ -1,40 +1,25 @@
-'''import xml.etree.ElementTree
-effort  = xml.etree.ElementTree.parse('change.xml').getroot()
-print(effort)'''
-
-'''import xmltodict
-with open('change.xml') as fd:
-    doc = xmltodict.parse(fd.read())
-print(change.xml)'''
-
-
+# Code to extract patent data from the XML files of the Full text data
 import xml.dom.minidom
 xmldoc= xml.dom.minidom.parse('fullpatenttext.xml')
 main=xmldoc.childNodes
 patent_title= xmldoc.getElementsByTagName("invention-title")
+patentgrant=xmldoc.getElementsByTagName("us-patent-grant")
+granted_date= patentgrant[0].attributes['date-produced'].value
 abstractstart=xmldoc.getElementsByTagName("abstract")
 abstract_text=abstractstart[0].getElementsByTagName("p")
+doc_number=xmldoc.getElementsByTagName('doc-number')
 claim=xmldoc.getElementsByTagName("claim-text")
-print(claim[0].childNodes)
-print("space")
-
-
-'''
-
-
 claimtext=claim[0].getElementsByTagName("claim-text")
-print(claimtext[0].nodeValue)
+firstline=xmldoc.getElementsByTagName("claim-text")
+ClaimText=firstline[0].childNodes[0].nodeValue
+for claimtext in claimtext:
+    finalclaimtext=first_claim_text = claimtext.childNodes[0].nodeValue
+    ClaimText=ClaimText + finalclaimtext
 
 
-first_claim_text=claimtext[0].firstChild
-print(first_claim_text)
-print(first_claim_text.nodeValue)
-
-
-'''
-
-
+print(doc_number[0].firstChild.nodeValue)
+print(granted_date)
 print(patent_title[0].firstChild.nodeValue)
 print(abstract_text[0].firstChild.nodeValue)
-
+print(ClaimText)
 
