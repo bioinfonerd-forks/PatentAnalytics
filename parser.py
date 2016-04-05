680e8de902c3a63e10c9f032a06814608821f4af
Nathan Roberson <nathaniroberson@gmail.com>
2016-04-04 20:16:45 -0400

    Extracting

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import xml.dom.minidom
 from config import Config
 import os
 
+
 class Patent(object):
     def __init__(self, properties):
         self.properties = properties
@@ -15,10 +16,10 @@ class Parser(object):
         filepath = os.path.join(config.data_dir, filename)
         xmldoc = xml.dom.minidom.parse(filepath)
         properties = dict()
-        properties['title'] = xmldoc.getElementsByTagName("invention-title")
+        properties['title'] = xmldoc.getElementsByTagName("invention-title")[0].childNodes[0].nodeValue
         properties['date'] = xmldoc.getElementsByTagName("us-patent-grant")[0].attributes['date-produced'].value
-        properties['abstract'] = xmldoc.getElementsByTagName("abstract")[0].getElementsByTagName("p")
-        properties['refnum'] = xmldoc.getElementsByTagName('doc-number')
+        properties['abstract'] = xmldoc.getElementsByTagName("abstract")[0].getElementsByTagName("p")[0].childNodes[0].nodeValue
+        properties['refnum'] = xmldoc.getElementsByTagName('doc-number')[0].childNodes[0].nodeValue
         properties['prefix'] = xmldoc.getElementsByTagName("claim-text")[0].childNodes[0].nodeValue
         claims = xmldoc.getElementsByTagName("claim-text")[0].getElementsByTagName("claim-text")
         properties['claims'] = [claim.childNodes[0].nodeValue for claim in claims]
