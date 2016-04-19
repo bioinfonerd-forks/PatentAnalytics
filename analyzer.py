8235021be7e9ba96d8006c47df32c53627b1f57c
Nathan Roberson <nathaniroberson@gmail.com>
2016-04-19 13:41:50 -0400

    Adding dataframe

--- analyzer.py
+++ analyzer.py
@@ -1,8 +1,10 @@
 from pandas import DataFrame
 
+
 class Analyzer(object):
     def __init__(self):
         pass
 
 if __name__ == "__main__":
     df = DataFrame.from_csv('data/2016Patent_Data.csv')
+    pass
