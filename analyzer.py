a33f8e6965b93f5acaefa7d404a5fe6b5cc44e0b
Billy Ermlick <ermlickw@vt.edu>
2016-04-19 14:32:32 -0400

    Creating factory and analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,11 @@
 from pandas import DataFrame
+import os
 
 
 class Analyzer(object):
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.config = config
 
-if __name__ == "__main__":
-    df = DataFrame.from_csv('data/2016Patent_Data.csv')
-    pass
+    def load_data(self):
+        df = DataFrame.from_csv(os.path.join(self.config.data_dir, '2016Patent_Data.csv'))
+        return df
---
+++ factory.py
@@ -0,0 +1,14 @@
+from config import Config
+from analyzer import Analyzer
+
+
+class Factory(object):
+    def __init__(self, config):
+        self.config = config
+        self.analyzer = Analyzer(config)
+
+
+if __name__ == '__main__':
+    config = Config()
+    f = Factory(config)
+    f.analyzer.load_data()
