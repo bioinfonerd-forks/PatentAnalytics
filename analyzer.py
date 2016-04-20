d2ff3ce7148e9cb870b2e60e2241d408d6aa7e7c
Billy Ermlick <ermlickw@vt.edu>
2016-04-19 23:38:04 -0400

    stop words have beaten me halp please

--- analyzer.py
+++ analyzer.py
@@ -9,13 +9,16 @@ class Analyzer(object):
         self.config = config
 
     def load_data(self):
+        w=1
         df = DataFrame.from_csv(os.path.join(self.config.data_dir, '2016Patent_Data.csv'))
         selected_data = df[((df.artunit.apply(str).str[:2] == "36") | (df.artunit.apply(str).str[:2] == "24") | (df.artunit.apply(str).str[:2] == "21"))]
+        selected_data = selected_data.reset_index(drop=True)
         stop_words = set(stopwords.words('english'))
-        for w in len(selected_data):
-            selected_data['abstract'][w] = selected_data['abstract'][w].apply(str)
-          #  something = word_tokenize(selected_data['abstract'][w])
-           # selected_data['abstract'][w] = [x for x in something if not x in stop_words]
+        text=[]
+        for w in range(len(selected_data)):
+            something = word_tokenize(selected_data['abstract'][w])
+            text[w]=[x for x in something if not x in stop_words]
+
         print(selected_data)
         print(stop_words)
         return selected_data
