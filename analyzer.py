d2a26fa6c84197abedbf272feb3f29b3ffe1e94b
Billy Ermlick <ermlickw@vt.edu>
2016-04-20 12:01:19 -0400

    stopwords fixed

--- analyzer.py
+++ analyzer.py
@@ -10,15 +10,15 @@ class Analyzer(object):
 
     def load_data(self):
         w=1
+        fulltext_without_stopwords = []
         df = DataFrame.from_csv(os.path.join(self.config.data_dir, '2016Patent_Data.csv'))
         selected_data = df[((df.artunit.apply(str).str[:2] == "36") | (df.artunit.apply(str).str[:2] == "24") | (df.artunit.apply(str).str[:2] == "21"))]
         selected_data = selected_data.reset_index(drop=True)
-        stop_words = set(stopwords.words('english'))
-        text=[]
+        stop_words = stopwords.words('english')
+        
         for w in range(len(selected_data)):
-            something = word_tokenize(selected_data['abstract'][w])
-            text[w]=[x for x in something if not x in stop_words]
-
+            fulltext_without_stopwords = ' '.join([x for x in word_tokenize(selected_data['abstract'][w]) if not x in stop_words])
+            selected_data.set_value('abstract',w,fulltext_without_stopwords)
         print(selected_data)
         print(stop_words)
         return selected_data
