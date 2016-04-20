b51e0c2a7eccdbdc6ff7933eabf59d731e890d01
Billy Ermlick <ermlickw@vt.edu>
2016-04-19 22:21:12 -0400

    trying out stop words

--- analyzer.py
+++ analyzer.py
@@ -1,8 +1,8 @@
 from pandas import DataFrame
 import os
-import re
 from nltk.corpus import stopwords
-# ...
+from nltk.tokenize import word_tokenize
+
 
 class Analyzer(object):
     def __init__(self, config):
@@ -10,7 +10,12 @@ class Analyzer(object):
 
     def load_data(self):
         df = DataFrame.from_csv(os.path.join(self.config.data_dir, '2016Patent_Data.csv'))
-        selected_data= df[((df.artunit.apply(str).str[:2]=="36") | (df.artunit.apply(str).str[:2]=="24") | (df.artunit.apply(str).str[:2]=="21"))]
-        filtered_words = [word for word in selected_data.artunit.apply(str)[5] if word not in stopwords.words('english')]
-        print(filtered_words)
-        return df
+        selected_data = df[((df.artunit.apply(str).str[:2] == "36") | (df.artunit.apply(str).str[:2] == "24") | (df.artunit.apply(str).str[:2] == "21"))]
+        stop_words = set(stopwords.words('english'))
+        for w in len(selected_data):
+            selected_data['abstract'][w] = selected_data['abstract'][w].apply(str)
+          #  something = word_tokenize(selected_data['abstract'][w])
+           # selected_data['abstract'][w] = [x for x in something if not x in stop_words]
+        print(selected_data)
+        print(stop_words)
+        return selected_data
