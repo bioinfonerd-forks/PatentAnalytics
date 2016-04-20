a57a333823ae8a8d5ef29a0e24cb2c292e4314f4
Billy Ermlick <ermlickw@vt.edu>
2016-04-19 21:00:42 -0400

    cleaning the data

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,8 @@
 from pandas import DataFrame
 import os
-
+import re
+from nltk.corpus import stopwords
+# ...
 
 class Analyzer(object):
     def __init__(self, config):
@@ -8,4 +10,7 @@ class Analyzer(object):
 
     def load_data(self):
         df = DataFrame.from_csv(os.path.join(self.config.data_dir, '2016Patent_Data.csv'))
+        selected_data= df[((df.artunit.apply(str).str[:2]=="36") | (df.artunit.apply(str).str[:2]=="24") | (df.artunit.apply(str).str[:2]=="21"))]
+        filtered_words = [word for word in selected_data.artunit.apply(str)[5] if word not in stopwords.words('english')]
+        print(filtered_words)
         return df
