3bec5054c275a45778a9a6a4873242d795e24b67
Nathan Roberson <nathaniroberson@gmail.com>
2016-04-27 23:46:14 -0400

    adding an app interface

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, session, g ,redirect, url_for, \
+    abort, render_template, send_from_directory
+
+from factory import RecommenderFactory
+from config import Config
+
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('query.html')
+
+
+@app.route('/query', methods=['POST', 'GET'])
+def submit_query():
+    config = Config(DEBUG)
+
+    keyword = None
+    object = None
+    predication = None
+
+    if request.method == 'POST':
+        # First search for subject
+        try:
+            abstract = request.form['abstract']
+        except KeyError:
+            error = 'No keyword entered or could be found'
+            return render_template('query.html', error=error)
+
+    return render_template('query.html')
+
+if __name__ == "__main__":
+    app.run()
