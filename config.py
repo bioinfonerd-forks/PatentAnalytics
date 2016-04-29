36bbedb79efd659c7b0e7ee685ccdc28186cdb52
Nathan Roberson <nathaniroberson@gmail.com>
2016-04-29 17:54:29 -0400

    uncomment

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Config(object):
         self.data_dir = os.path.join(self.base_dir, 'data')
         self.model_name = '_feature_model.dill'
         self.matrix_name = '_feature_matrix.dill'
-        self.art_units =  (
+        self.art_units = (
             "36", "24", "21",
-             # "16", "17", "26", "28", "29", "37"
+            "16", "17", "26", "28", "29", "37"
         )
 
     def get_model_path(self, feature_name):
