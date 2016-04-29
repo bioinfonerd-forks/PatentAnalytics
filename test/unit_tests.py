b4d9ffa0fe8f9d065398a6ad0a16e1a216f2c39f
Nathan Roberson <nathaniroberson@gmail.com>
2016-04-29 14:11:14 -0400

    cannot compute

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -1,9 +1,18 @@
 import unittest
+import json
+
 
 class UnitTests(unittest.TestCase):
 
     def test_predict(self):
+        pass
 
 
-if __name__=='__main__':
+def load_test_data():
+    return json.load('test_data.json')
+
+if __name__ == '__main__':
+    test_data = load_test_data()
     unittest.main()
+
+
