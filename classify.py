b4c94c63ded7c238476b50c56aafc25ca007166c
Nathan Roberson <nathaniroberson@gmail.com>
2016-05-02 13:13:05 -0400

    fixins

--- classify.py
+++ classify.py
@@ -62,9 +62,9 @@ class Classify(object):
         :return:
         """
         classifiers = {
-            'knn':[KNeighborsClassifier(), {'n_neighbors': [1, 2, 3, 4, 5]}],
-            'bayes':[MultinomialNB(),{'alpha': [0.01, 0.02, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.5, 1]}]
-            'sgd':[SGDClassifier(), {'alpha': [0.001, 0.005, 0.01, 0.05, 0.1],
+            # 'knn':[KNeighborsClassifier(), {'n_neighbors': [1, 2, 3, 4, 5]}],
+            'bayes': [MultinomialNB(),{'alpha': [0.01, 0.02, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.5, 1]}],
+            'sgd': [SGDClassifier(), {'alpha': [0.001, 0.005, 0.01, 0.05, 0.1],
              'n_iter': [1, 2, 5, 8, 10]}]
         }
 
