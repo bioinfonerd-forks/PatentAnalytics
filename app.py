98ec31822237553a5b4f6659047951a752162d85
William D Ermlick <ermlickw@vt.edu>
2016-05-23 13:29:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,6 @@ def submit_query():
 
 if __name__ == '__main__':
     from os import environ
-    port=environ.get("PORT", 5000))
+    port=environ.get("PORT", 5000)
     app.run(host='0.0.0.0', port=port)
     #app.run()
